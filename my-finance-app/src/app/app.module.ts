9b669d72aaf54e7135604962f5946b62d2033721
ConstantinRusenko <krusenko@gmail.com>
2022-04-30 15:22:15 +0300

    update app module file

--- my-finance-app/src/app/app.module.ts
+++ my-finance-app/src/app/app.module.ts
@@ -17,7 +17,6 @@ import { FrontPageItemComponent } from './components/front-page-item/front-page-
 import { FillAmountDirective } from './directives/fill-amount.directive';
 import { FillCostDirective } from './directives/fill-cost.directive';
 import { AddNewComponent } from './components/add-new/add-new.component';
-import {HttpClientModule} from "@angular/common/http";
 
 const appRoutes: Routes = [
   { path: 'archive', component: TransactionArchiveComponent },
