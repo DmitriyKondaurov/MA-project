524de6ab12883aab5959884359a074c05593f052
ConstantinRusenko <krusenko@gmail.com>
2022-05-02 21:16:37 +0300

    add colorize card for archive transaction

--- my-finance-app/src/app/components/cost-monitoring/statistic/statistic.component.ts
+++ my-finance-app/src/app/components/cost-monitoring/statistic/statistic.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RestApiService } from '../../../services/res-api.service'
 
 @Component({
   selector: 'app-statistic',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StatisticComponent implements OnInit {
 
-  constructor() { }
+  data: string[] = [];
+
+  constructor(private RestApiService: RestApiService) { }
 
   ngOnInit(): void {
+    this.RestApiService.getTest().subscribe(res => {
+      this.data = res;
+      // this.transitions = this.MonitoringInfoService.monitoringInfo(this.data);
+    })
   }
 
 }
