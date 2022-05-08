71fcb1b45ed203818f7cb6ff5c83a64c1e094042
Dmytro Kondaurov <invisibl@ukr.net>
2022-05-08 21:22:46 +0300

    fix color schema

--- my-finance-app/src/app/components/menu/menu.component.ts
+++ my-finance-app/src/app/components/menu/menu.component.ts
@@ -1,5 +1,6 @@
 import {Component, ElementRef, HostBinding, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
+import {Expenses} from "../../app-interfaces";
 
 @Component({
   selector: 'app-menu',
@@ -10,15 +11,35 @@ export class MenuComponent {
   selected: string = '';
   status: boolean = false;
 
-  elements: string[] = [
-    'Main page',
-    'Costs analysis',
-    'Category',
-    'Planned budget',
-    'Last transaction',
-    'Plan/Actual report',
-    'Add new',
-    'Goals'
+  elements: Expenses[] = [
+    {
+      title: 'Main page',
+      value: ''
+    },
+    {
+      title: 'Costs analysis',
+      value: 'monitoring'
+    },
+    {
+      title: 'Category',
+      value: ''
+    },
+    {
+      title: 'Planned budget',
+      value: ''
+    },
+    {
+      title: 'Last transaction',
+      value: 'archive'
+    },
+    {
+      title: 'Plan/Actual report',
+      value: ''
+    },
+    {
+      title: 'Goals',
+      value: ''
+    },
   ];
   constructor(private router: Router, private element: ElementRef) { }
 
@@ -29,9 +50,9 @@ export class MenuComponent {
   ngOnInit(): void {
   }
 
-  select(item: string) {
-    this.selected = item;
-    // this.router.navigateByUrl(`portal/${item}`);
+  select(item: Expenses) {
+    this.selected = item.title;
+    this.router.navigateByUrl(item.value).then();
   }
 
   hideMenu(): void {
